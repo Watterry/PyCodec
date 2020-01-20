112d1aea2aacf8fea63d90096c44d33003f4d809
cookwhy <cookwhy@163.com>
2020-01-20 15:49:17 +0800

    refine the code style of NaluStreamer

--- modules/NaluStreamer.py
+++ modules/NaluStreamer.py
@@ -1,42 +1,60 @@
-from bitstring import BitStream
+# Based on the document of ITU-T Recommendation H.264 05/2003 edition
+# 
+
+from bitstring import BitStream, BitArray
 from h26x_extractor import nalutypes
 
+START_CODE_PREFIX = '0x00000001'
+START_CODE_PREFIX_SHORT = "0x000001"
 
 class NaluStreamer():
 
-    def __init__(self):
-        START_CODE_PREFIX = '0x00000001'
-        START_CODE_PREFIX_SHORT = "0x000001"
+    def __init__(self, nalu_type):
+        '''
+        init the value of nalu unit
+        : param nalu_type: something like NAL_UNIT_TYPE_CODED_SLICE_IDR in nalutypes.py
+        '''
+        self.forbidden_zero_bit = '0b0'
+        self.nal_ref_idc = '0b11'
+        self.nal_unit_type = "0b" + "{0:05b}".format(nalu_type)
+
         self.stream = BitStream(START_CODE_PREFIX)
 
     def export(self, bitstream_outputfile):
         """
         output the binary data into file
         """
+        self.stream.append(self.forbidden_zero_bit)
+        self.stream.append(self.nal_ref_idc)
+        self.stream.append(self.nal_unit_type)
+
+        #output to the file
         f = open(bitstream_outputfile, 'wb')
         self.stream.tofile(f)
+        f.close()
 
-class SPSStreamer():
+class SPSStreamer(NaluStreamer):
     """
     Sequence Parameter Set class
+    Based on 7.3.2.1 setion on page 31 & 7.4.2.1 setion on page 53
     """
 
-    def __init__(self):
+    def __init__(self, nalu_type):
+        super().__init__(nalu_type)
 
         # initializers
-        self.offset_for_ref_frame = []
-        # self.seq_scaling_list_present_flag = []
+        #self.offset_for_ref_frame = []
+        #self.seq_scaling_list_present_flag = []
 
-        # self.profile_idc = self.s.read('uint:8')
-        # self.constraint_set0_flag = self.s.read('uint:1')
-        # self.constraint_set1_flag = self.s.read('uint:1')
-        # self.constraint_set2_flag = self.s.read('uint:1')
-        # self.constraint_set3_flag = self.s.read('uint:1')
-        # self.constraint_set4_flag = self.s.read('uint:1')
-        # self.constraint_set5_flag = self.s.read('uint:1')
-        # self.reserved_zero_2bits = self.s.read('uint:2')
-        # self.level_idc = self.s.read('uint:8')
-        # self.seq_parameter_set_id = self.s.read('ue')
+        # use some default value
+        self.profile_idc = '0x64'   # u(8)
+        self.constraint_set0_flag = '0b0' # u(1)
+        self.constraint_set1_flag = '0b0' # u(1)
+        self.constraint_set2_flag = '0b0' # u(1)
+
+        self.reserved_zero_2bits = '0b00000' # u(5)
+        self.level_idc = '0b00001' # u(5)
+        self.seq_parameter_set_id = '0b0' #ue(v)
 
         # if ((self.profile_idc == 100) or (self.profile_idc == 110) or (self.profile_idc == 122) or (self.profile_idc == 144)):
         #     self.chroma_format_idc = self.s.read('ue')
@@ -52,13 +70,13 @@ class SPSStreamer():
         #         # TODO: have to implement this, otherwise it will fail
         #         raise NotImplementedError("Scaling matrix decoding is not implemented.")
 
-        # self.log2_max_frame_num_minus4 = self.s.read('ue')
-        # self.pic_order_cnt_type = self.s.read('ue')
+        self.log2_max_frame_num_minus4 = '0b1' #ue(v)
+        self.pic_order_cnt_type = '0b1' #ue(v)
 
         # if self.pic_order_cnt_type == 0:
-        #     self.log2_max_pic_order_cnt_lsb_minus4 = self.s.read('ue')
+        #     self.log2_max_pic_order_cnt_lsb_minus4 = '0b1' #ue(1), TODO
         # elif self.pic_order_cnt_type == 1:
-        #     self.delta_pic_order_always_zero_flag = self.s.read('uint:1')
+        #     self.delta_pic_order_always_zero_flag = '0b1' # u(1)
         #     self.offset_for_non_ref_pic = self.s.read('se')
         #     self.offset_for_top_to_bottom_filed = self.s.read('se')
         #     self.num_ref_frames_in_pic_order_cnt_cycle = self.s.read('ue')
@@ -86,9 +104,53 @@ class SPSStreamer():
 
         # self.print_verbose()
 
+    def set_level_idc(self, level_number):
+        '''
+        set level_idc in SPS, foramt: u(8)
+        : param set_id: int number of set id, according to Table A-1 Level limits on page 207
+        '''
+        self.level_idc = "0b" + "{0:08b}".format(level_number*10)
+        print(self.level_idc)
+
+    def set_seq_parameter_set_id(self, set_id):
+        '''
+        set seq_parameter_set_id in SPS, foramt: ue(v)
+        : param set_id: int number of set id
+        '''
+        s = BitArray(ue=set_id)
+        self.seq_parameter_set_id = s
+        print(self.seq_parameter_set_id)
+
+    def set_log2_max_frame_num_minus4(self, value):
+        '''
+        set log2_max_frame_num_minus4 in SPS, foramt: ue(v)
+        : param value: described on page 54
+        '''
+        s = BitArray(ue=value)
+        self.log2_max_frame_num_minus4 = s
+        print(self.log2_max_frame_num_minus4)
+
+    def export(self, bitstream_outputfile):
+        """
+        output the binary data into file
+        """
+        self.stream.append(self.profile_idc)
+        self.stream.append(self.constraint_set0_flag)
+        self.stream.append(self.constraint_set1_flag)
+        self.stream.append(self.constraint_set2_flag)
+        self.stream.append(self.reserved_zero_2bits)
+        self.stream.append(self.level_idc)
+        self.stream.append(self.seq_parameter_set_id)
+
+        super().export(bitstream_outputfile)
+
 def main():
     f = "E:/temp/output/nalustreamer.264"
-    ex = NaluStreamer()
+    ex = SPSStreamer(nalutypes.NAL_UNIT_TYPE_SPS)
+    ex.profile_idc = '0x42'
+    ex.set_level_idc(3) # level 3
+    ex.set_seq_parameter_set_id(0)
+    ex.set_log2_max_frame_num_minus4(0)
     ex.export(f)
 
 if __name__ == '__main__':
