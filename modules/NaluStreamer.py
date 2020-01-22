0899405f32a17ac807dd661e450d485cdfd25bf9
cookwhy <cookwhy@163.com>
2020-01-22 15:10:18 +0800

    finish the basic SPS generator

--- modules/NaluStreamer.py
+++ modules/NaluStreamer.py
@@ -19,14 +19,30 @@ class NaluStreamer():
         self.nal_unit_type = "0b" + "{0:05b}".format(nalu_type)
 
         self.stream = BitStream(START_CODE_PREFIX)
+        self.stream.append(self.forbidden_zero_bit)
+        self.stream.append(self.nal_ref_idc)
+        self.stream.append(self.nal_unit_type)
+
+    def rbsp_trailing_bits(self):
+        '''
+        according to RBSP trainling bits syntax on page 35, and according to explanation page 49
+        '''
+        rbsp_stop_one_bit = '0b1'
+        rbsp_alignment_zero_bit = '0b0'
+        self.stream.append(rbsp_stop_one_bit)
+
+        plus = 8 - (self.stream.length % 8)
+
+        for x in range(0, plus):
+            self.stream.append(rbsp_alignment_zero_bit)
+
+        #print("length after plus:")
+        #print(self.stream.length)
 
     def export(self, bitstream_outputfile):
         """
         output the binary data into file
         """
-        self.stream.append(self.forbidden_zero_bit)
-        self.stream.append(self.nal_ref_idc)
-        self.stream.append(self.nal_unit_type)
 
         #output to the file
         f = open(bitstream_outputfile, 'wb')
@@ -37,6 +53,8 @@ class SPSStreamer(NaluStreamer):
     """
     Sequence Parameter Set class
     Based on 7.3.2.1 setion on page 31 & 7.4.2.1 setion on page 53
+    The sequence of set__ function is not important.
+    the function export() will take care of the sequence of the SODB.
     """
 
     def __init__(self, nalu_type):
@@ -73,9 +91,65 @@ class SPSStreamer(NaluStreamer):
         self.log2_max_frame_num_minus4 = '0b1' #ue(v)
         self.pic_order_cnt_type = '0b1' #ue(v)
 
-        # if self.pic_order_cnt_type == 0:
-        #     self.log2_max_pic_order_cnt_lsb_minus4 = '0b1' #ue(1), TODO
-        # elif self.pic_order_cnt_type == 1:
+        self.num_ref_frames = '0b1' #ue(v)
+        self.gaps_in_frame_num_value_allowed_flag = '0b0' #u(1)
+        self.pic_width_in_mbs_minus_1 = '0b0' #u(ue)
+        self.pic_height_in_map_units_minus_1 = '0b0' #u(ue)
+        self.frame_mbs_only_flag = '0b1' #u(1)
+        # if not self.frame_mbs_only_flag:
+        #     self.mb_adapative_frame_field_flag = self.s.read('uint:1')
+        self.direct_8x8_inference_flag = '0b0' #u(1)
+        self.frame_cropping_flag = '0b0' #u(1)
+        # if self.frame_cropping_flag:
+        #     self.frame_crop_left_offst = self.s.read('ue')
+        #     self.frame_crop_right_offset = self.s.read('ue')
+        #     self.frame_crop_top_offset = self.s.read('ue')
+        #     self.frame_crop_bottom_offset = self.s.read('ue')
+        self.vui_parameters_present_flag = '0b0' #u(1)
+
+        # # TODO: parse VUI
+        # #self.rbsp_stop_one_bit         = self.s.read('uint:1')
+
+        # self.print_verbose()
+
+    def set__level_idc(self, level_number):
+        '''
+        set level_idc in SPS, foramt: u(8)
+        : param set_id: int number of set id, according to Table A-1 Level limits on page 207
+        '''
+        self.level_idc = "0b" + "{0:08b}".format(level_number*10)
+
+    def set__seq_parameter_set_id(self, set_id):
+        '''
+        set seq_parameter_set_id in SPS, foramt: ue(v)
+        : param set_id: int number of set id
+        '''
+        s = BitArray(ue=set_id)
+        self.seq_parameter_set_id = s
+
+    def set__log2_max_frame_num_minus4(self, value):
+        '''
+        set log2_max_frame_num_minus4 in SPS, foramt: ue(v)
+        : param value: described on page 54
+        '''
+        s = BitArray(ue=value)
+        self.log2_max_frame_num_minus4 = s
+
+    def set__pic_order_cnt_type(self, value):
+        '''
+        set pic_order_cnt_type in SPS, foramt: ue(v)
+        : param value: described on page 54
+        '''
+        s = BitArray(ue=value)
+        self.pic_order_cnt_type = s
+        print(self.pic_order_cnt_type)
+        if (value == 0):
+            # log2_max_pic_order_cnt_lsb_minus4
+            log2_max_pic_order_cnt_lsb_minus4 = 0   # todo, need to add specific data
+            temp = BitArray(ue=log2_max_pic_order_cnt_lsb_minus4)
+            self.pic_order_cnt_type.append(temp)
+        elif (value == 1):
+            print("TODO: ADD MORE SPECIFIC ITEM")
         #     self.delta_pic_order_always_zero_flag = '0b1' # u(1)
         #     self.offset_for_non_ref_pic = self.s.read('se')
         #     self.offset_for_top_to_bottom_filed = self.s.read('se')
@@ -83,56 +157,98 @@ class SPSStreamer(NaluStreamer):
         #     for i in range(self.num_ref_frames_in_pic_order_cnt_cycle):
         #         self.offset_for_ref_frame.append(self.s.read('se'))
 
-        # self.num_ref_frames = self.s.read('ue')
-        # self.gaps_in_frame_num_value_allowed_flag = self.s.read('uint:1')
-        # self.pic_width_in_mbs_minus_1 = self.s.read('ue')
-        # self.pic_height_in_map_units_minus_1 = self.s.read('ue')
-        # self.frame_mbs_only_flag = self.s.read('uint:1')
-        # if not self.frame_mbs_only_flag:
-        #     self.mb_adapative_frame_field_flag = self.s.read('uint:1')
-        # self.direct_8x8_inference_flag = self.s.read('uint:1')
-        # self.frame_cropping_flag = self.s.read('uint:1')
-        # if self.frame_cropping_flag:
-        #     self.frame_crop_left_offst = self.s.read('ue')
-        #     self.frame_crop_right_offset = self.s.read('ue')
-        #     self.frame_crop_top_offset = self.s.read('ue')
-        #     self.frame_crop_bottom_offset = self.s.read('ue')
-        # self.vui_parameters_present_flag = self.s.read('uint:1')
-
-        # # TODO: parse VUI
-        # #self.rbsp_stop_one_bit         = self.s.read('uint:1')
-
-        # self.print_verbose()
-
-    def set_level_idc(self, level_number):
+    def set__num_ref_frames(self, value):
         '''
-        set level_idc in SPS, foramt: u(8)
-        : param set_id: int number of set id, according to Table A-1 Level limits on page 207
-        '''
-        self.level_idc = "0b" + "{0:08b}".format(level_number*10)
-        print(self.level_idc)
-
-    def set_seq_parameter_set_id(self, set_id):
-        '''
-        set seq_parameter_set_id in SPS, foramt: ue(v)
-        : param set_id: int number of set id
-        '''
-        s = BitArray(ue=set_id)
-        self.seq_parameter_set_id = s
-        print(self.seq_parameter_set_id)
-
-    def set_log2_max_frame_num_minus4(self, value):
-        '''
-        set log2_max_frame_num_minus4 in SPS, foramt: ue(v)
-        : param value: described on page 54
+        set num_ref_frames in SPS, foramt: ue(v)
+        : param value: described on page 55
         '''
         s = BitArray(ue=value)
-        self.log2_max_frame_num_minus4 = s
-        print(self.log2_max_frame_num_minus4)
+        self.num_ref_frames = s
+
+    def set__gaps_in_frame_num_value_allowed_flag(self, bool_value):
+        '''
+        set gaps_in_frame_num_value_allowed_flag in SPS, foramt: u(1)
+        : param value: described on page 55
+        '''
+        if (bool_value):
+            self.gaps_in_frame_num_value_allowed_flag = '0b1'
+        else:
+            self.gaps_in_frame_num_value_allowed_flag = '0b0'
+
+    def set__pic_width_in_mbs_minus_1(self, width):
+        '''
+        set pic_width_in_mbs_minus_1 in SPS, foramt: ue(v)
+        : param width: video width, according to page 55 formula 7-3, 7-4, 7-5
+        '''
+        MB_width = 16
+        pic_width_in_mbs_minus_1 = int( width/MB_width - 1 )
+        s = BitArray(ue=pic_width_in_mbs_minus_1)
+        self.pic_width_in_mbs_minus_1 = s
+
+    def set__pic_height_in_map_units_minus1(self, height):
+        '''
+        set pic_height_in_map_units_minus1 in SPS, foramt: ue(v)
+        : param width: video height, according to page 55 formula 7-6, 7-7
+        '''
+        MB_height = 16
+        pic_height_in_map_units_minus_1 = int( height/MB_height - 1 )
+        s = BitArray(ue=pic_height_in_map_units_minus_1)
+        self.pic_height_in_map_units_minus_1 = s
+
+    def set__frame_mbs_only_flag(self, bool_value):
+        '''
+        set frame_mbs_only_flag in SPS, foramt: ue(1)
+        : param bool_value: have or not have field slices or field MBs, according to page 55
+        '''
+        if (bool_value):
+            self.frame_mbs_only_flag = '0b1'
+        else:
+            self.frame_mbs_only_flag = '0b0'
+            # todo: add mb_adaptive_frame_field_flag
+            #mb_adaptive_frame_field_flag = '0b0' #u(1) TODO
+            # self.frame_mbs_only_flag.append(mb_adaptive_frame_field_flag) #TODO
+
+    def set__direct_8x8_inference_flag(self, bool_value):
+        '''
+        set direct_8x8_inference_flag in SPS, foramt: ue(v)
+        : param bool_value: Specifies how certain B macroblock motion vectors are derived on page 55
+        '''
+        if (bool_value):
+            self.direct_8x8_inference_flag = '0b1'
+        else:
+            self.direct_8x8_inference_flag = '0b0'
+
+    def set__frame_cropping_flag(self, bool_value):
+        '''
+        set frame_cropping_flag in SPS, foramt: ue(v)
+        : param bool_value: Specifies how certain B macroblock motion vectors are derived on page 55
+        '''
+        if (bool_value):
+            self.frame_cropping_flag = '0b1'
+            # TODO add more flags
+            # frame_crop_left_offset
+            # frame_crop_left_offset
+            # frame_crop_left_offset
+            # frame_crop_bottom_offset
+        else:
+            self.frame_cropping_flag = '0b0'
+
+    def set__vui_parameters_present_flag(self, bool_value):
+        '''
+        set vui_parameters_present_flag in SPS, foramt: ue(v)
+        : param bool_value: Specifies how certain B macroblock motion vectors are derived on page 55
+        '''
+        if (bool_value):
+            self.vui_parameters_present_flag = '0b1'
+            # TODO add more flags
+            # vui_parameters_present_flag
+        else:
+            self.vui_parameters_present_flag = '0b0'
 
     def export(self, bitstream_outputfile):
         """
         output the binary data into file
+        The sequence here is very important, should be exact the same as SPECIFIC of H.264
         """
         self.stream.append(self.profile_idc)
         self.stream.append(self.constraint_set0_flag)
@@ -141,17 +257,38 @@ class SPSStreamer(NaluStreamer):
         self.stream.append(self.reserved_zero_2bits)
         self.stream.append(self.level_idc)
         self.stream.append(self.seq_parameter_set_id)
+        self.stream.append(self.log2_max_frame_num_minus4)
+        self.stream.append(self.pic_order_cnt_type)
+        self.stream.append(self.num_ref_frames)
+        self.stream.append(self.gaps_in_frame_num_value_allowed_flag)
+        self.stream.append(self.pic_width_in_mbs_minus_1)
+        self.stream.append(self.pic_height_in_map_units_minus_1)
+        self.stream.append(self.frame_mbs_only_flag)
+        self.stream.append(self.direct_8x8_inference_flag)
+        self.stream.append(self.frame_cropping_flag)
+        self.stream.append(self.vui_parameters_present_flag)
+        super().rbsp_trailing_bits()
 
         super().export(bitstream_outputfile)
 
 def main():
     f = "E:/temp/output/nalustreamer.264"
-    ex = SPSStreamer(nalutypes.NAL_UNIT_TYPE_SPS)
-    ex.profile_idc = '0x42'
-    ex.set_level_idc(3) # level 3
-    ex.set_seq_parameter_set_id(0)
-    ex.set_log2_max_frame_num_minus4(0)
-    ex.export(f)
+    sps = SPSStreamer(nalutypes.NAL_UNIT_TYPE_SPS)
+    sps.profile_idc = '0x42'
+    sps.set__level_idc(3) # level 3
+    sps.set__seq_parameter_set_id(0)
+    sps.set__log2_max_frame_num_minus4(0)
+    sps.set__pic_order_cnt_type(0)
+    sps.set__num_ref_frames(10)
+    sps.set__gaps_in_frame_num_value_allowed_flag(False)
+    sps.set__pic_width_in_mbs_minus_1(512)
+    sps.set__pic_height_in_map_units_minus1(512)
+    sps.set__frame_mbs_only_flag(True)
+    sps.set__direct_8x8_inference_flag(True)
+    sps.set__frame_cropping_flag(False)
+    sps.set__vui_parameters_present_flag(False)
+    
+    sps.export(f)
 
 if __name__ == '__main__':
     main()
