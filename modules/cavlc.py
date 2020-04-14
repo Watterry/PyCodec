eeca255a5c9b35ebe6009fd44fabd09da4033fc3
cookwhy <cookwhy@163.com>
2020-04-14 19:43:20 +0800

    add license header

--- modules/cavlc.py
+++ modules/cavlc.py
@@ -1,4 +1,20 @@
-# The coding and decoding of encoder
+# The coding and decoding of H.264 CAVLC
+#
+# Copyright (C) <2020>  <cookwhy@qq.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import numpy as np
 from bitstring import BitStream, BitArray
 import ZigZag
