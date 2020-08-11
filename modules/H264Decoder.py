3afca668cee7621653f875f11910befb5e420f4c
cookwhy <cookwhy@163.com>
2020-08-11 20:10:04 +0800

    use h26x_parser directly instead of pip installed package

--- modules/H264Decoder.py
+++ modules/H264Decoder.py
@@ -16,57 +16,40 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
-from h26x_extractor import h26x_parser
+# there is something wrong using h26x_extractor installed package
+# so including h26x source code directly
+#from h26x_extractor import h26x_parser
+import h26x_parser
+
 from bitstring import BitStream, BitArray
 from NaluParser import *
 import prediction
 
+sps_parser = SpsParser()
+pps_parser = PpsParser()
+nal_parser = NalParser()
+
 def get_sps(bytes):
     # do something with the NALU bytes
+    logging.debug("get sps")
     logging.debug(bytes)
+    sps_parser.parse( BitStream(bytes) )
 
-def do_something(bytes):
+def get_pps(bytes):
     # do something with the NALU bytes
+    logging.debug("get pps")
     logging.debug(bytes)
+    pps_parser.parse( BitStream(bytes) )
 
-def main(h264file):
-    """
-    Args:
-        h264file: h264file name, should be using suffix .264 o .h264
-    """
-    # Test Case 1: use test data with one macroblock directly, hard code binary data
-    #sps = BitStream('0x42c01edb02004190')
-    #pps = BitStream('0xca83cb20')
-    #nal = BitStream('0x88843f0a60109e4400020ed2fd431c63a895f346c35c5f92408f38eae8430cc80c8abc765961')
+def get_aud(bytes):
+    # do something with the NALU bytes
+    logging.debug("aud")
+    return bytes
 
-    # Test Case 2: read H264 binary code from file
+def get_slice(bytes):
+    # do something with the NALU bytes
+    logging.debug("slice")
 
-    # Option1: use H264Parser to read file directly
-    # TODO: something wrong with h264parser, need to fix later
-    # h264parser = h26x_parser.H26xParser(h264file)
-    # h264parser.set_callback("sps", get_sps)
-    # h264parser.set_callback("nalu", do_something)
-    # h264parser.parse()
-
-    # Option2: use position directly for test
-    b = BitArray(bytes=open(h264file, 'rb').read())
-    sps = BitStream(b[5*8: 13*8])
-    logging.debug("sps: %s", sps)
-   
-    pps = BitStream(b[18*8: 22*8])
-    logging.debug("pps: %s", pps)
-    
-    nal = BitStream(b[26*8: b.len])
-    logging.debug("data: %s", nal[0:800])
-
-    # do the decoding things
-    sps_parser = SpsParser()
-    sps_parser.parse(sps)
-
-    pps_parser = PpsParser()
-    pps_parser.parse(pps)
-
-    nal_parser = NalParser()
     residual, modemap = nal_parser.parse(nal, sps_parser, pps_parser)
     
     image = prediction.inverseIntraPrediction(residual, modemap, 16)
@@ -85,6 +68,34 @@ def main(h264file):
 
     plt.show()
 
+def get_nalu(bytes):
+    # do something with the NALU bytes
+    #logging.debug(bytes)
+    logging.debug("nalu bytes")
+
+def main(h264file):
+    """
+    Args:
+        h264file: h264file name, should be using suffix .264 o .h264
+    """
+    # Test Case 1: use test data with one macroblock directly, hard code binary data
+    #sps = BitStream('0x42c01edb02004190')
+    #pps = BitStream('0xca83cb20')
+    #nal = BitStream('0x88843f0a60109e4400020ed2fd431c63a895f346c35c5f92408f38eae8430cc80c8abc765961')
+
+    # Test Case 2: read H264 binary code from file
+
+    # Option1: use H264Parser to read file directly
+    # TODO: something wrong with h264parser, need to fix later
+    h264parser = h26x_parser.H26xParser(h264file)
+    #h264parser.set_callback("nalu", do_something)
+    h264parser.set_callback("sps", get_sps)
+    h264parser.set_callback("pps", get_pps)
+    h264parser.set_callback("aud", get_aud)
+    h264parser.set_callback("slice", get_slice)
+    h264parser.set_callback("nalu", get_nalu)
+    h264parser.parse()
+
 if __name__ == '__main__':
     logging.basicConfig(
         level=logging.DEBUG,
@@ -96,4 +107,5 @@ if __name__ == '__main__':
     )
     logging.getLogger('matplotlib.font_manager').disabled = True
 
-    main("../test/lena_x264_baseline_I_16x16.264")
+    main("E:/liumangxuxu/code/PyCodec/test/BasketballPass_720p.264")
+    #main("E:/liumangxuxu/code/PyCodec/test/lena_x264_baseline_I_16x16.264")
